c96a522e300b7ea375806fcea881b02b973f30b5
islam elgahlan <islam.elgahlan@gmail.com>
2022-10-23 12:51:03 +0200

    make it better2

--- configuration/configurationDB.js
+++ configuration/configurationDB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connection = () => {
     return mongoose
-    .connect(process.env.connection_string)
+    .connect(process.env.connection_string_dev_atlas)
     .then((result) => console.log("db connected"))
     .catch((error) => console.log(error))
 }
